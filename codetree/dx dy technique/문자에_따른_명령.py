726846069645bae0a3fbc2608326422f6a337d1b
dorahee-ee <wonnn0801@hufs.ac.kr>
2023-07-25 15:50:09 +0900

    HUFS 코딩테스트 대비 캠프 0725

---
+++ codetree/dx dy technique/문자에_따른_명령.py
@@ -0,0 +1,14 @@
+commands = input()
+x, y = 0, 0
+dir_num = 3
+dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
+
+for command in commands:
+    if command == 'L':
+        dir_num = (dir_num - 1 + 4) % 4
+    elif command == 'R':
+        dir_num = (dir_num + 1) % 4
+    else:
+        x, y = x + dx[dir_num], y + dy[dir_num]
+
+print(x, y)
---
+++ codetree/dx dy technique/방향에_맞춰_이동.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+x, y = 0, 0
+# 동, 서, 남, 북순으로 dx, dy 정의
+dx, dy = [1, -1, 0, 0], [0, 0, -1, 1]
+mapper = {'E':0, 'W':1, 'S':2, 'N':3}
+
+for i in range(n):
+    move_dir, dist = input().split()
+    dist = int(dist)
+    move_dir = mapper[move_dir]
+
+    # 주어진 방향대로 dist 거리만큼 이동
+    x += dx[move_dir] * dist
+    y += dy[move_dir] * dist
+
+    print(x, y)
